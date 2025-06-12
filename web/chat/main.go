628ffc1210653d6216f6937a9e4e2aba123a31de
Artem Zakirullin <artemzr@gmail.com>
2025-06-12 09:32:08 +0300

    add wasm chat

---
+++ web/chat/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"syscall/js"
+)
+
+func hi(this js.Value, args []js.Value) interface{} {
+	return "Hello, world!"
+}
+
+func main() {
+	js.Global().Set("hi", js.FuncOf(hi))
+
+	select {}
+}
