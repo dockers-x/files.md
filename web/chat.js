fa24d80d330eee692c42bacb760ec5f377c02241
Artem Zakirullin <artemzr@gmail.com>
2025-06-14 13:03:32 +0300

    fix img preview padding

--- web/chat.js
+++ web/chat.js
@@ -543,12 +543,9 @@ async function saveFile(fileName, file) {
 }
 
 function generateSafeFileName(originalName) {
-    return originalName
-    // todo fix unsafe
     const now = new Date();
     const timestamp = `${String(now.getDate()).padStart(2, '0')}.${String(now.getMonth() + 1).padStart(2, '0')}.${now.getFullYear()} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}`;
-    const safeName = (originalName || 'untitled').replace(/[<>:"/\\|?*\s]/g, '-');
-    return `${timestamp}-${safeName}`;
+    return `${timestamp}-${originalName}`.replace(/[<>:"/\\|?*\s]/g, '-');
 }
 
 input.addEventListener('paste', async (event) => {
