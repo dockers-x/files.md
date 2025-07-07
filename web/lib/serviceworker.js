d17a2e942d43c8033a7efa82d3050ed9f1dbf447
Artem Zakirullin <artemzr@gmail.com>
2025-07-07 14:01:43 +0300

    add all files to service worker

--- web/lib/serviceworker.js
+++ web/lib/serviceworker.js
@@ -1,8 +1,42 @@
 const CACHE_NAME = 'files-md-v1';
 const urlsToCache = [
-    '/',           // Instead of '/'
-    '/chat.wasm',  // Instead of 'chat.wasm'
-    'chat.wasm'
+    '/',
+    '/favicon.ico',
+    '/manifest.json',
+    '/app.css?v=',
+    '/lib/normalize.css?v=',
+    '/lib/sidebar.css?v=',
+    '/lib/codemirror.css?v=',
+    '/lib/hypermd.css?v=',
+    '/lib/theme-light.css?v=',
+    '/lib/theme-dark.css?v=',
+    '/chat.css?v=',
+    '/lib/sidebar.js?v=',
+    '/lib/codemirror.js?v=',
+    '/lib/core.js?v=',
+    '/lib/markdown.js?v=',
+    '/lib/hypermd.js?v=',
+    '/lib/keymap.js?v=',
+    '/lib/click.js?v=',
+    '/lib/hide-token.js?v=',
+    '/lib/fold.js?v=',
+    '/lib/fold-image.js?v=',
+    '/lib/fold-link.js?v=',
+    '/lib/table-align.js?v=',
+    '/lib/autocomplete-link.js?v=',
+    '/lib/show-hint.js?v=',
+    '/lib/autoscroll.js?v=',
+    '/lib/codemirror-go.js?v=',
+    '/lib/codemirror-php.js?v=',
+    '/lib/codemirror-shell.js?v=',
+    '/lib/similarity.js?v=',
+    '/welcome.js?v=',
+    '/files.js?v=',
+    '/wasm_exec.js?v=',
+    '/app.js?v=',
+    '/wasm.js?v=',
+    '/chat.js?v=',
+    '/modals.js?v=',
 ];
 
 self.addEventListener('install', event => {
